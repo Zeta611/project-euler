8d51368a554cd247acd7260f4384df28367c494e
Jaeho Lee <jaeho.lee@snu.ac.kr>
2020-02-24 01:04:03 +0900

    :poop: Improve 049

--- 049-prime-permutations.c
+++ 049-prime-permutations.c
@@ -19,133 +19,24 @@ void generate_sieve(bool* sieve, int cnt)
 
 bool check_permutations(int a, int b)
 {
-	int a_1 = a % 10;
-	int a_2 = a / 10 % 10;
-	int a_3 = a / 100 % 10;
-	int a_4 = a / 1000 % 10;
-	int b_1 = b % 10;
-	int b_2 = b / 10 % 10;
-	int b_3 = b / 100 % 10;
-	int b_4 = b / 1000 % 10;
-	if (a_1 == b_1) {
-		if (a_2 == b_2) {
-			if (a_3 == b_3) {
-				return a_4 == b_4;
-			} else if (a_3 == b_4) {
-				return a_4 == b_3;
-			} else {
-				return false;
-			}
-		} else if (a_2 == b_3) {
-			if (a_3 == b_2) {
-				return a_4 == b_4;
-			} else if (a_3 == b_4) {
-				return a_4 == b_2;
-			} else {
-				return false;
-			}
-		} else if (a_2 == b_4) {
-			if (a_3 == b_2) {
-				return a_4 == b_3;
-			} else if (a_3 == b_3) {
-				return a_4 == b_2;
-			} else {
-				return false;
-			}
-		} else {
-			return false;
-		}
-	} else if (a_1 == b_2) {
-		if (a_2 == b_1) {
-			if (a_3 == b_3) {
-				return a_4 == b_4;
-			} else if (a_3 == b_4) {
-				return a_4 == b_3;
-			} else {
-				return false;
-			}
-		} else if (a_2 == b_3) {
-			if (a_3 == b_1) {
-				return a_4 == b_4;
-			} else if (a_3 == b_4) {
-				return a_4 == b_1;
-			} else {
-				return false;
-			}
-		} else if (a_2 == b_4) {
-			if (a_3 == b_1) {
-				return a_4 == b_3;
-			} else if (a_3 == b_3) {
-				return a_4 == b_1;
-			} else {
-				return false;
-			}
-		} else {
-			return false;
-		}
-	} else if (a_1 == b_3) {
-		if (a_2 == b_1) {
-			if (a_3 == b_2) {
-				return a_4 == b_4;
-			} else if (a_3 == b_4) {
-				return a_4 == b_2;
-			} else {
-				return false;
-			}
-		} else if (a_2 == b_2) {
-			if (a_3 == b_1) {
-				return a_4 == b_4;
-			} else if (a_3 == b_4) {
-				return a_4 == b_1;
-			} else {
-				return false;
-			}
-		} else if (a_2 == b_4) {
-			if (a_3 == b_1) {
-				return a_4 == b_2;
-			} else if (a_3 == b_2) {
-				return a_4 == b_4;
-			} else {
-				return false;
-			}
-		} else {
-			return false;
-		}
-	} else if (a_1 == b_4) {
-		if (a_2 == b_1) {
-			if (a_3 == b_2) {
-				return a_4 == b_3;
-			} else if (a_3 == b_3) {
-				return a_4 == b_2;
-			} else {
-				return false;
-			}
-		} else if (a_2 == b_2) {
-			if (a_3 == b_1) {
-				return a_4 == b_1;
-			} else if (a_3 == b_3) {
-				return a_4 == b_3;
-			} else {
-				return false;
-			}
-		} else if (a_2 == b_3) {
-			if (a_3 == b_1) {
-				return a_4 == b_2;
-			} else if (a_3 == b_2) {
-				return a_4 == b_1;
-			} else {
-				return false;
-			}
-		} else {
-			return false;
-		}
-	} else {
-		return false;
+	int digits[10] = {0};
+	int i;
+
+	for (i = 0; i < 4; ++i) {
+		++digits[a % 10];
+		--digits[b % 10];
+		a /= 10;
+		b /= 10;
 	}
+
+	for (i = 0; i < 10; ++i) {
+		if (digits[i]) return false;
+	}
+	return true;
 }
 
 
-bool check_three_permutations(int a, int b, int c)
+static inline bool check_three_permutations(int a, int b, int c)
 {
 	return check_permutations(a, b) && check_permutations(a, c);
 }
@@ -188,6 +79,7 @@ int main(void)
 	clock_t end = clock();
 	float seconds = (float)(end - start) / CLOCKS_PER_SEC;
 	printf("Took %f seconds\n", seconds);
+	/** 0.005172 seconds */
 
 	return 0;
 }
